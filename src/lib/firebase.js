baa594d7def2e3826375f91e80d7a3292f49da1d
Priyanshu <priyanshu.khadka100@gmail.com>
2026-08-11 13:57:50 +0545

    fix

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,14 +4,14 @@ import 'firebase/compat/auth';
 import 'firebase/compat/database';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCyIzNTCeID1uWfIw_17QC5ciUBqtHqNkE',
-  authDomain: 'findafriend-535b3.firebaseapp.com',
-  databaseURL: 'https://findafriend-535b3-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'findafriend-535b3',
-  storageBucket: 'findafriend-535b3.firebasestorage.app',
-  messagingSenderId: '789997283271',
-  appId: '1:789997283271:web:5cb2c1c3c50d9f248d9be2',
-  measurementId: 'G-TEGX90V5V8',
+  apiKey: 'AIzaSyC2Cj1nXJQ6MnV3NQOM-NVlI6Da1GWIT8I',
+  authDomain: 'findafriend-2.firebaseapp.com',
+  databaseURL: 'https://findafriend-2-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'findafriend-2',
+  storageBucket: 'findafriend-2.firebasestorage.app',
+  messagingSenderId: '681290347001',
+  appId: '1:681290347001:web:b4989566229fbfa8168d28',
+  measurementId: 'G-M4X5S10KYX',
 };
 
 export { firebaseConfig };
